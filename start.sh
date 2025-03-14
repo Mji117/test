d313f51dbc7a954c60b431aac8967e240e0247d4
Mji117 <testobmk4@gmail.com>
2025-03-14 12:53:59 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-nginx
 ffmpeg -re -i "https://uselector.cdn.intigral-ott.net/MB2H/MB2H.isml/manifest.mpd" \
        -decryption_key 58fa20cf6055f3aefc15707992c0b685 \
-       -c:v copy -c:a copy -f flv rtmp://localhost/live/stream
+       -c:v copy -c:a copy -f flv rtmp://localhost:3000/live/stream
