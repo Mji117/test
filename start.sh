c23c9e47bde783c5d89834a9670318d73a7e0407
Mji117 <testobmk4@gmail.com>
2025-04-20 10:23:10 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,3 +0,0 @@
-ffmpeg -re -i "https://uselector.cdn.intigral-ott.net/MB2H/MB2H.isml/manifest.mpd" \
-       -decryption_key 58fa20cf6055f3aefc15707992c0b685 \
-       -c:v copy -c:a copy -f flv rtmp://localhost:3000/live/stream
